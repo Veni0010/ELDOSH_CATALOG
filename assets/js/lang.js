ed504f6aac8ba611352ef1439ad032795775c2ab
Veni0010 <117179819+Veni0010@users.noreply.github.com>
2025-02-07 14:01:14 +0500

    Add files via upload

---
+++ assets/js/lang.js
@@ -0,0 +1,26 @@
+// const langArr = {
+//     "unit":  {
+//         "uz": "bosh sahifa",
+//         "ru": "Главная",
+//     }, 
+//     "chip": {
+//         "ru": "графический чип",
+//         "uz": "graphics processing unit",
+//     }, 
+//     "memory": {
+//         "ru": "объем памяти",
+//         "uz": "memory size",
+//     }, 
+//     "memory-type": {
+//         "ru": "тип памяти",
+//         "uz": "memory type",
+//     }, 
+//     "cool": {
+//         "ru": "тип системы охлаждения",
+//         "uz": "сooling system type",
+//     }, 
+//     "more": {
+//         "ru": "детально о товаре",
+//         "uz": "more details",
+//     }, 
+// }
